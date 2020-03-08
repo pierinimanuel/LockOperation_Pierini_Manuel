2d0d8bb962db140b10e59841c4a8be3b8a9e590c
manuel.melchiorri7846 <manuel.melchiorri7846@hotmail.com>
2020-03-08 18:33:15 +0100

    inizio esercizio

---
+++ Aumento/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aumento
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 0;
+			object padlock = new object();
+
+			for (int i = 0; i < 5; i++)
+			{
+				Task conta = new Task(() =>
+				{
+					// Monitor
+
+
+
+
+
+				});
+			}
+		}
+	}
+}
